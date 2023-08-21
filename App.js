de761fad21850d5de66fcb39a8d835bc7fc9adc6
Harsh Dobariya <harshdobariya79@gmail.com>
2023-08-21 09:08:48 +0530

    Initial setup

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Home = () => {
+    return (
+        <div>LJ Connect frontend.</div>
+    )
+}
+
+// Defining the root of the document
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Adding container element to the root, all the content inside the root will be replaced
+root.render(<Home />);
